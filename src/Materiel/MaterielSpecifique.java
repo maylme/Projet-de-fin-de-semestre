b3092d24a4e2f13599e4754614f0cebbe2167565
Lyameina <lyameina@Paulette.(none)>
2014-01-20 11:11:46 +0100

    debut decoupage

---
+++ src/Materiel/MaterielSpecifique.java
@@ -0,0 +1,13 @@
+package Materiel;
+
+/**
+ * Defini le materiel Specifique:
+ * Certains types de matériel très spécifiques 
+ * existent dans le stock à peu d'exemplaires, 
+ * ils servent pour des projets ou des expérimentations
+ * @author lyameina
+ *
+ */
+public class MaterielSpecifique extends TypeDeMateriel{
+
+}
