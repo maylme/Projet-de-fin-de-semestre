eb6726b0cde446a8349b35b63834ef1d117483de
Riana Rabehasy <rabehasy@z052.(none)>
2014-01-23 14:47:03 +0100

    Méthode afficherEmprunts modifiée A CAUSE DE SONIA TUAL

---
+++ src/Materiel/MaterielEleve.java
@@ -0,0 +1,27 @@
+package Materiel;
+
+import java.util.Date;
+
+import utilisateurs.Eleve;
+import utilisateurs.Emprunteur;
+
+@SuppressWarnings("serial")
+public class MaterielEleve extends Materiel{
+    public MaterielEleve(Caracteristiques c, int dureeMax, int nbExemplaires) {
+        super(c,dureeMax,nbExemplaires);
+    }
+    public MaterielEleve(Caracteristiques c, int nbExemplaires) {
+        super(c, nbExemplaires);
+    }
+    public MaterielEleve(Caracteristiques c) {
+        super(c);
+    }
+    
+    @Override
+    public boolean empruntable(Date dateDebut, Date dateFin, Emprunteur e ){
+        if (super.empruntable(dateDebut, dateFin, e)) {
+            return (e instanceof Eleve);
+        }
+        return false;
+    }
+}
